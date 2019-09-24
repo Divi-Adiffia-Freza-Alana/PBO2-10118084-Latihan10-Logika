2c1a0fa0e9749d4608962503f968fd7318ac8b69
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:39:17 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118084/latihan10/logika/PBO210118084Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan10.logika;
+
+/**
+ *
+ * @author
+ * Nama  : Divi Adiffia Freza Alana
+ * Kelas : PBO2
+ * Nim   : 10118084
+ * Deskripsi Program : Program ini berisi program untuk menampilkan teks
+
+ */
+public class PBO210118084Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
